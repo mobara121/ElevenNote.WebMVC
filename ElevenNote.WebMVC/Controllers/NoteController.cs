c47a0f4175449041538bd7bdd625bdd3997f61de
mobara121 <mizueo121@gmail.com>
2020-09-29 14:57:48 -0400

    refactor index view for NoteListItem.model

---
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,20 @@
+﻿using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    [Authorize]  //Controllerの前のNoteが、localhost:xxx/Note
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index() // localhost:xxx/Note/IndexにいくとViewを返してくれる。
+        {
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+    }
+}
